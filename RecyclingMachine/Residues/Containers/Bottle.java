1962b4f69e31291c902113bf158d639af7502257
Shockp <masque2204@gmail.com>
2024-12-22 18:23:41 +0100

    Bottle class implemented

--- RecyclingMachine/Residues/Containers/Bottle.java
+++ RecyclingMachine/Residues/Containers/Bottle.java
@@ -1,5 +1,28 @@
 package RecyclingMachine.Residues.Containers;
 
-public class Bottle {
-	
+import RecyclingMachine.Residues.Container;
+
+/**
+ * Abstract class representing a bottle, which is a type of container.
+ * A bottle is a specific kind of container and extends the {@link Container} class.
+ */
+public abstract class Bottle extends Container {
+	/**
+	 * Constructor to initialize a bottle with a price and weight.
+	 * 
+	 * @param price the price of the bottle
+	 * @param weight the weight of the bottle
+	 */
+	public Bottle(double price, double weight) {
+		super(price, weight);
+	}
+
+	/**
+	 * Abstract method to return the residue type of the bottle.
+	 * The residue type will be specific to this class.
+	 * 
+	 * @return the type of residue for the bottle
+	 */
+	@Override
+	public abstract String getResidueType();
 }
