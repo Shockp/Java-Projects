8e3749613b79357a800077e1e458cd9f191a3f21
Shockp <masque2204@gmail.com>
2025-01-01 18:14:49 +0100

    Machine prototype

--- RecyclingMachine/MachineCore/Machine.java
+++ RecyclingMachine/MachineCore/Machine.java
@@ -1,5 +1,39 @@
 package RecyclingMachine.MachineCore;
 
+import java.util.ArrayList;
+
+import RecyclingMachine.UserInterface.*;
+import RecyclingMachine.Residues.*;
+
 public class Machine {
-	
+    private double money;
+    private ArrayList<User> users;
+
+    public User registerUser() {
+        User user = newUser();
+    }
+
+    public User identify() {
+
+    }
+
+    public void printPublicRecycledResidues() {
+
+    }
+
+    public void menu() {
+
+    }
+
+    private User newUser() {
+
+    }
+
+    private User isRegistered() {
+
+    }
+
+    private int requestNumber() {
+
+    }
 }
