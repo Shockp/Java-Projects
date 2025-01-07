158170a773f38e94971af095ad06b384cc3baa26
shockp <masque2204@gmail.com>
2025-01-07 21:28:46 +0100

    Removing the comments in ManagementFDP

--- ManagementFDP/FPDcore/ManagementPlatform.java
+++ ManagementFDP/FPDcore/ManagementPlatform.java
@@ -8,15 +8,6 @@ import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class ManagementPlatform {
-    /*
-     * Dos listas una con los FDP inscritos y otra con los FDP finalizados.
-     * 
-     * Método sobresalientes() imprime un fichero de texto la información de todos los FDP
-     * con una calificación mayor o igual a 9.
-     * 
-     * Método ordenados() que guarde un fichero binario de todos los FPD finalizados
-     * y ordenados por calificación.
-     */
     private ArrayList<FDP> enrolledList;
     private ArrayList<FDP> finishedList;
 
